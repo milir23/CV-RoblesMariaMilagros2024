69bd3f38baba24c745379423aedafa41817d0df0
milagrosr <milagros.r23@gmail.com>
2024-01-09 13:17:25 -0300

    modificacion de src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,12 +9,25 @@
 //   </React.StrictMode>,
 // )
 
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+// Utiliza ReactDOM.createRoot para renderizar la aplicación en el elemento con id 'root'
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // Utiliza React.StrictMode para activar un conjunto de comprobaciones adicionales en desarrollo
   <React.StrictMode>
     <App />
   </React.StrictMode>,
