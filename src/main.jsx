493917e548a78091616d3509c7ef6b8141d0b4d2
milagrosr <milagros.r23@gmail.com>
2024-01-09 13:35:34 -0300

    modificaciones en main e index

--- src/main.jsx
+++ src/main.jsx
@@ -25,9 +25,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-// Utiliza ReactDOM.createRoot para renderizar la aplicación en el elemento con id 'root'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // Utiliza React.StrictMode para activar un conjunto de comprobaciones adicionales en desarrollo
+
   <React.StrictMode>
     <App />
   </React.StrictMode>,
