8ea2f2c209bb6240be12d4777db749c155de3b01
milagrosr <milagros.r23@gmail.com>
2024-01-26 12:20:36 -0300

    :modificacion en Proyectos.jsx

---
+++ src/components/Proyectos/Proyectos.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+const Proyectos = () => {
+    return (
+        <div>
+            <section>
+                <div>
+                    <h2> Proyectos
+                    </h2>
+                </div>
+            </section>
+            <section>
+          <div className="recuadro1">
+            <div className="proyecto1">Proyecto 1</div>
+            <div className="recuadro2">
+              <div className="espacio">
+                <p>/**</p>
+                <p>* Lenguaje HTML CSS y JavaScript</p>
+                <p>* Manejo de ReactJS</p>
+                <p>* Vite</p>
+                <p>* Bootstrap <br />*/</p>
+              </div>
+              <div className='espaciobtn'>
+                <p>
+                  <a href="#" className="btn1">Ver más</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        </div>
+
+    );
+};
+
+export default Proyectos;
